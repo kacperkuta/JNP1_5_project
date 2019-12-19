a426ff03a15b1f320ec719f1484006b3e8d18e23
Kacper Kuta <k.kuta2@student.uw.edu.pl>
2019-12-19 21:22:55 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <functional>
+
+#define MAP insertion_ordered_map
+
+template <class K, class V, class Hash = std::hash<K>>
+class MAP {
+public:
+
+
+
+
+private:
+    typedef struct node {
+
+
+    } node;
+    size_t size;
+
+};
