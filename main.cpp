f9129dbc5a9030bb1fda627e9036eab7f36a770c
Kacper Kuta <k.kuta2@student.uw.edu.pl>
2019-12-19 21:43:23 +0100

    First scheme

--- main.cpp
+++ main.cpp
@@ -1,20 +1,45 @@
 #include <cstdlib>
 #include <functional>
+#include <memory>
 
 #define MAP insertion_ordered_map
 
 template <class K, class V, class Hash = std::hash<K>>
 class MAP {
 public:
+    MAP () : my_size(0), tab(new node*[16]) {};
 
+    MAP (const MAP& other)  {
+        my_size = other.size();
+        tab = other.get_tab();
+    }
 
+    size_t size() noexcept {
+        return my_size;
+    }
 
+protected:
+    typedef struct node {
+        K key;
+        V val;
+        node * next;
+        node * link;
+        node * back_link;
+    } node;
+
+    using tab_ptr = std::shared_ptr<node*[]>;
+
+    tab_ptr get_tab() {
+        return tab;
+    }
 
 private:
-    typedef struct node {
 
-
-    } node;
-    size_t size;
+    size_t my_size;
+    tab_ptr tab;
 
 };
+
+int main() {
+
+}
